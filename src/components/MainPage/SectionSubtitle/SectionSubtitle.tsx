d70bfaad820994c072f7f1d6f4b37a241e6a23ac
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2025-04-29 14:47:53 +0300

    upd

--- src/components/MainPage/SectionSubtitle/SectionSubtitle.tsx
+++ src/components/MainPage/SectionSubtitle/SectionSubtitle.tsx
@@ -2,8 +2,13 @@ import { FC } from 'react';
 import { Subtitle } from './SectionSubtitle.styled';
 import { IProps } from './SectionSubtitle.types';
 
-const SectionSubtitle: FC<IProps> = ({ text,maxWidth }) => {
-  return <Subtitle maxWidth={maxWidth}>{text}</Subtitle>;
+const SectionSubtitle: FC<IProps> = ({ text, maxWidth }) => {
+  return (
+    <Subtitle
+      maxWidth={maxWidth}
+      dangerouslySetInnerHTML={{ __html: text }}
+    ></Subtitle>
+  );
 };
 
 export default SectionSubtitle;
